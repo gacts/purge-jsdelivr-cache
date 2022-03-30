5f3966e9e68a32136e24ff4d21aca15087737b8b
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-03-30 19:03:49 +0500

    Core wrote

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const core = require('@actions/core');
 
-core.info((new Date()).toTimeString());
+// read action inputs
+const input = {
+  url: core.getInput('url', {required: true}),
+  attempts: core.getInput('attempts'),
+}
+
+// main action entrypoint
+async function run() {
+  core.info(input.url)
+  core.info(input.attempts)
+}
+
+// run the action
+try {
+  run()
+} catch (error) {
+  core.setFailed(error.message)
+}
